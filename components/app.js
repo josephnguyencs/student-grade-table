c9fa52bcffaed0ecb91c92402c0ed228da94d554
Joseph Nguyen <josephsportycs@gmail.com>
2020-07-31 12:02:12 -0700

    Completed the add funcitonality of the student grade table. 3 new methods were added and the data is being added as well to the dom whenever the add button is clicked.

--- components/app.js
+++ components/app.js
@@ -5,6 +5,9 @@ class App {
     this.gradeForm = gradeForm
     this.handleGetGradesError = this.handleGetGradesError.bind(this)
     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this)
+    this.createGrade = this.createGrade.bind(this)
+    this.handleCreateGradeError = this.handleCreateGradeError.bind(this)
+    this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this)
   }
   handleGetGradesError(error) {
     console.error(error)
@@ -33,6 +36,33 @@ class App {
     })
   }
   start() {
+    console.log(this.gradeForm)
+    this.getGrades()
+    this.gradeForm.onSubmit(this.createGrade)
+  }
+  createGrade(name, course, grade) {
+    console.log(name)
+    console.log(course)
+    console.log(grade)
+    $.ajax({
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      method: "POST",
+      headers: {
+        "X-Access-Token": "V3nsU11k"
+      },
+      data: {
+        "name": name,
+        "course": course,
+        "grade": grade
+      },
+      success: this.handleCreateGradeSuccess,
+      error: this.handleCreateGradeError
+    })
+  }
+  handleCreateGradeError(error) {
+    console.error(error)
+  }
+  handleCreateGradeSuccess() {
     this.getGrades()
   }
 }
--- components/grade-form.js
+++ components/grade-form.js
@@ -2,9 +2,11 @@ class GradeForm {
   constructor(formElement) {
     this.handleSubmit = this.handleSubmit.bind(this)
     this.formElement = formElement
+    this.formElement.addEventListener('submit', this.handleSubmit)
   }
   onSubmit(createGrade) {
     this.createGrade = createGrade
+    console.log(createGrade)
   }
   handleSubmit(event) {
     event.preventDefault()
@@ -14,6 +16,6 @@ class GradeForm {
     var grade = parseInt(formData.get('grade'))
     this.createGrade(name, course, grade)
     event.target.reset()
-    console.log("hi")
+    console.log(event.target)
   }
 }
